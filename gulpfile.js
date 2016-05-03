3111449ee2b79d5d9674459640fafef0480a1ab9
Misael Cázares <misael.upg@gmail.com>
2016-05-03 19:11:17 +0000

    Static Star Pojects Section Completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-
-gulp.task('styles', function() {
+var sourcemaps = require('gulp-sourcemaps');
+gulp.task('styles', function() {    
     gulp.src('scss/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('./css/'))
 });
 
